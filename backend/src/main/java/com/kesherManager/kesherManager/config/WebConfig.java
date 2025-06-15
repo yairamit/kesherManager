26acc538c44cee48868642b8a606a520308e70cb
yairamit <62564451+yairamit@users.noreply.github.com>
2025-06-15 08:44:41 +0300

    Update WebConfig.java

--- backend/src/main/java/com/kesherManager/kesherManager/config/WebConfig.java
+++ backend/src/main/java/com/kesherManager/kesherManager/config/WebConfig.java
@@ -1,10 +1,6 @@
 package com.kesherManager.kesherManager.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,7 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://keshermanager-frontend.onrender.com/")
-                .allowedMethods("*");
+                .allowedOrigins(
+                    "http://localhost:3000",
+                    "https://keshermanager-frontend.onrender.com"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
